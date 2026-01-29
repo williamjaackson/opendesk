78ec10141cb158f94010e17c2adb1098474d17a2
William Jackson <william@jaackson.com>
2026-01-30 06:41:31 +1000

    Add new organisation form with create action

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -2,4 +2,25 @@ class OrganisationsController < ApplicationController
   def index
     @organisations = Current.user.organisations
   end
+
+  def new
+    @organisation = Organisation.new
+  end
+
+  def create
+    @organisation = Organisation.new(organisation_params)
+
+    if @organisation.save
+      @organisation.users << Current.user
+      redirect_to organisations_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def organisation_params
+    params.require(:organisation).permit(:name)
+  end
 end
