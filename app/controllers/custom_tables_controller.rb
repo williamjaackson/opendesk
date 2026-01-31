91b4fccc8aa008938c48773f73b4908547b6306c
William Jackson <william@jaackson.com>
2026-01-31 12:10:04 +1000

    Add edit page with field management, shared form partial, and delete support

--- app/controllers/custom_tables_controller.rb
+++ app/controllers/custom_tables_controller.rb
@@ -5,20 +5,50 @@ class CustomTablesController < ApplicationController
     @custom_table = Current.organisation.custom_tables.find(params[:id])
     @fields = @custom_table.custom_fields.order(:position)
     @records = @custom_table.custom_records.includes(custom_values: :custom_field)
+
+    if params[:query].present?
+      matching_ids = CustomValue.where(custom_field: @fields)
+        .where("value LIKE ?", "%#{params[:query]}%")
+        .select(:custom_record_id)
+      @records = @records.where(id: matching_ids)
+    end
   end
 
   def new
     @custom_table = Current.organisation.custom_tables.new
   end
 
+  def edit
+    @custom_table = Current.organisation.custom_tables.find(params[:id])
+    @fields = @custom_table.custom_fields.order(:position)
+    @fields = @fields.where("name LIKE ?", "%#{params[:query]}%") if params[:query].present?
+  end
+
   def create
     @custom_table = Current.organisation.custom_tables.new(custom_table_params)
 
     if @custom_table.save
-      redirect_to custom_table_path(@custom_table)
+      redirect_to edit_custom_table_path(@custom_table)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def update
+    @custom_table = Current.organisation.custom_tables.find(params[:id])
+
+    if @custom_table.update(custom_table_params)
+      redirect_to custom_table_path(@custom_table)
+    else
+      @fields = @custom_table.custom_fields.order(:position)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @custom_table = Current.organisation.custom_tables.find(params[:id])
+    @custom_table.destroy
+    redirect_to dashboard_path
   end
 
   private
