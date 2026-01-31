f114366cb2d9c45a6fcffbfeda01071f4c755bf9
William Jackson <william@jaackson.com>
2026-02-01 05:28:34 +1000

    Toggle edit mode via fetch so dropdown stays open

--- app/controllers/edit_modes_controller.rb
+++ app/controllers/edit_modes_controller.rb
@@ -1,6 +1,6 @@
 class EditModesController < ApplicationController
   def update
     session[:edit_mode] = !session[:edit_mode]
-    redirect_back fallback_location: root_path
+    head :no_content
   end
 end
---
+++ app/javascript/controllers/edit_mode_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  toggle() {
+    const token = document.querySelector('meta[name="csrf-token"]').content
+    fetch(this.element.dataset.editModeUrlValue, {
+      method: "PATCH",
+      headers: {
+        "X-CSRF-Token": token,
+        "Accept": "text/html"
+      }
+    })
+    document.body.toggleAttribute("data-edit-mode")
+  }
+}
