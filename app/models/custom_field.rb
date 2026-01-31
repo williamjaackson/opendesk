6fade485e876a916c6c5183cfb689cf6e20e9f60
William Jackson <william@jaackson.com>
2026-01-31 12:15:52 +1000

    Delete empty records when a custom field is destroyed

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -7,4 +7,12 @@ class CustomField < ApplicationRecord
 
   validates :name, presence: true
   validates :field_type, presence: true, inclusion: { in: FIELD_TYPES }
+
+  after_destroy :cleanup_empty_records
+
+  private
+
+  def cleanup_empty_records
+    custom_table.custom_records.left_joins(:custom_values).where(custom_values: { id: nil }).destroy_all
+  end
 end
