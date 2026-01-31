44362a31a329c8a0539188a9c9f180308455ffa5
William Jackson <william@jaackson.com>
2026-02-01 05:43:00 +1000

    Redirect dashboard to first table, show empty state when none exist

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
   def show
+    first_table = Current.organisation&.custom_tables&.order(:position)&.first
+
+    if first_table
+      redirect_to custom_table_path(first_table)
+    end
   end
 end
