fdbdcf20ca5b94ffdf7af623e785216765346686
William Jackson <william@jaackson.com>
2026-01-31 15:54:59 +1000

    Feature | Custom checkbox partial with Stimulus controller (#33)

---
+++ app/javascript/controllers/checkbox_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["input", "box"]
+
+  toggle() {
+    this.inputTarget.value = this.inputTarget.value === "1" ? "0" : "1"
+    this.render()
+  }
+
+  keydown(event) {
+    if (event.key === " " || event.key === "Enter") {
+      event.preventDefault()
+      this.toggle()
+    }
+  }
+
+  render() {
+    const checked = this.inputTarget.value === "1"
+    this.boxTarget.classList.toggle("bg-gray-900", checked)
+    this.boxTarget.classList.toggle("border-gray-900", checked)
+    this.boxTarget.classList.toggle("border-gray-300", !checked)
+    this.boxTarget.querySelector("[data-checkmark]").classList.toggle("invisible", !checked)
+  }
+}
