b6c99271de10b20a9341e55413ffef2efd62c641
William Jackson <william@jaackson.com>
2026-02-01 05:24:47 +1000

    Rename EditModeController to EditModesController to match resource route

--- app/controllers/edit_modes_controller.rb
+++ app/controllers/edit_modes_controller.rb
@@ -1,4 +1,4 @@
-class EditModeController < ApplicationController
+class EditModesController < ApplicationController
   def update
     session[:edit_mode] = !session[:edit_mode]
     redirect_back fallback_location: root_path
