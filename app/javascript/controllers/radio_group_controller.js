aedb4a0d7026b5b8595e9983ff2d23e22fdf2f0b
William Jackson <william@jaackson.com>
2026-02-02 02:57:56 +1000

    Feature | Add custom radio button component (#61)

---
+++ app/javascript/controllers/radio_group_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["input", "option"]
+
+  select(event) {
+    const value = event.currentTarget.dataset.value
+    this.inputTarget.value = value
+    this.render()
+    this.inputTarget.dispatchEvent(new Event("change", { bubbles: true }))
+  }
+
+  keydown(event) {
+    if (event.key === " " || event.key === "Enter") {
+      event.preventDefault()
+      this.select(event)
+    }
+  }
+
+  render() {
+    const selected = this.inputTarget.value
+    this.optionTargets.forEach(option => {
+      const isSelected = option.dataset.value === selected
+      const dot = option.querySelector("[data-dot]")
+      const ring = option.querySelector("[data-ring]")
+
+      ring.classList.toggle("border-gray-900", isSelected)
+      ring.classList.toggle("border-gray-300", !isSelected)
+      dot.classList.toggle("invisible", !isSelected)
+    })
+  }
+}
