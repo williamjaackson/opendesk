50308a7e79480982cfffb41d75482624e3ae3a4b
William Jackson <william@jaackson.com>
2026-02-03 17:40:58 +1000

    Add CsvImportJob for background processing

---
+++ app/jobs/csv_import_job.rb
@@ -0,0 +1,14 @@
+class CsvImportJob < ApplicationJob
+  queue_as :default
+
+  def perform(csv_import_id)
+    csv_import = CsvImport.find(csv_import_id)
+    return unless csv_import.processing?
+
+    importer = CsvImporter.new(csv_import)
+    importer.import_all
+  rescue StandardError => e
+    csv_import.update!(status: "failed", errors_log: [ { row: 0, message: e.message } ])
+    raise
+  end
+end
