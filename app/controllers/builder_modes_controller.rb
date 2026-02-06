2642c27b92800ecebcb8f5cc25ee85c2a7fe258a
William Jackson <william@jaackson.com>
2026-02-06 14:42:30 +1000

    Restrict builder mode to admin users only

--- app/controllers/builder_modes_controller.rb
+++ app/controllers/builder_modes_controller.rb
@@ -1,6 +1,19 @@
 class BuilderModesController < ApplicationController
   def update
+    unless current_membership&.admin?
+      head :forbidden
+      return
+    end
+
     session[:builder_mode] = !session[:builder_mode]
     head :no_content
+  end
+
+  private
+
+  def current_membership
+    return unless Current.organisation
+
+    Current.organisation.organisation_users.find_by(user: Current.user)
   end
 end
