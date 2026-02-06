293d6ea7180ce5d13b8a794e5824b9f5397e70ac
William Jackson <william@jaackson.com>
2026-02-06 12:20:53 +1000

    Feature | Account settings page (#111)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   has_many :organisation_users, dependent: :destroy
   has_many :organisations, through: :organisation_users
 
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_04_024729) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_06_020631) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -179,6 +179,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_04_024729) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
+    t.string "name"
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
